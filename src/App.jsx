db42e4f41efae158cfd41c03cd85b93d03da3edd
Yujin <0505ujin@naver.com>
2025-07-22 16:45:49 +0900

    component-header(미완)
    only letter
    just for test

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 //import { useState } from 'react'
 import './App.css'
 import Nav from './components/Nav';
+import Header from './components/Header';
 
 function App() {
 
   return (
     <>
       <div>
+
         <Nav></Nav>
       </div>
     </>
---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+function Header(){
+    return (
+        <>
+        <div className="Header">
+            <h1>NaviDate</h1>
+            <p>icon 들어갈거임</p>
+
+        </div>
+        </>
+    )
+}
+
+export default Header;
