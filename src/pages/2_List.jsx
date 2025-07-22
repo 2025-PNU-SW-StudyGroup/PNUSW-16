229909bca1af755004522293f6c0b75fd77bf79b
hyunjinch <hjin24@pusan.ac.kr>
2025-07-22 17:55:59 +0900

    List.css 추가

---
+++ src/pages/2_List.jsx
@@ -0,0 +1,18 @@
+//import { useState } from 'react'
+import './App.css'
+import Nav from './components/Nav';
+import Header from './components/Header';
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <Header></Header>
+        <Nav></Nav>
+      </div>
+    </>
+  )
+}
+
+export default App
