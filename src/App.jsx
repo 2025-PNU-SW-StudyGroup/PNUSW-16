eb41273f7527e139ab4f2cea7c7ace3aae7533fd
Yujin <0505ujin@naver.com>
2025-07-24 02:27:29 +0900

    page: Map 지도 구현
    kakao api 지도 구현

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,15 @@ import './App.css'
 import Nav from './components/Nav';
 import Header from './components/Header';
 import Home from './pages/1_Home';
+import Map from './pages/3_map';
 
 function App() {
 
   return (
     <>
       <div>
-        <Home />
+        {/* <Home /> */}
+        <Map />
       </div>
     </>
   )
