504c47f461cd9f93c7300223fc89af15165edee5
Yujin <0505ujin@naver.com>
2025-07-24 15:06:23 +0900

    app.jsx(for resolve conflict)

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,17 @@
 //import { useState } from 'react'
 import './App.css'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Login from './pages/Signup/Login';
-import Signup1 from './pages/Signup/Signup1';
-import Signup2 from './pages/Signup/Signup2';
-import Signup3 from './pages/Signup/Signup3';
+import Home from './pages/1_Home';
+import KakaoMap from './pages/3_map';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/signup1" element={<Signup1 />} />
-        <Route path="/signup2" element={<Signup2 />} />
-        <Route path="/signup3" element={<Signup3 />} />
-      </Routes>
-    </Router>
-  );
+    <>
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/map' element={<KakaoMap />} />
+    </Routes>
+    </>
+  )
 }
 
 export default App
