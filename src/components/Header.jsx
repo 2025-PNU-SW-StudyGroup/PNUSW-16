21ef3336b4dd95548950086320c3795ca3249822
Yujin <0505ujin@naver.com>
2025-07-24 14:12:02 +0900

    nav 바, header icon 코드 수정

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,9 +4,8 @@ function Header(){
     return (
         <>
         <div className="header_div">
-            <link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined" rel="stylesheet" />
             <h1>NaviDate</h1>
-            <span class="material-symbols-outlined">notifications</span>
+            <span className="material-symbols-outlined">notifications</span>
         </div>
         </>
     )
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -5,18 +5,17 @@ function Nav(){
     return(
         <nav>
             <div className="nav_div">
-                <link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined" rel="stylesheet" />
                 <div className="nav_part">
-                    <span class="material-symbols-outlined">home</span>
+                    <span className="material-symbols-outlined">home</span>
                 </div>
                 <div className="nav_part">
-                    <span class="material-symbols-outlined">list</span>
+                    <span className="material-symbols-outlined">list</span>
                 </div>
                 <div className="nav_part">
-                    <span class="material-symbols-outlined">map</span>
+                    <span className="material-symbols-outlined">map</span>
                 </div>
                 <div className="nav_part">
-                    <span class="material-symbols-outlined">person</span>
+                    <span className="material-symbols-outlined">person</span>
                 </div>
             </div>
         </nav>
